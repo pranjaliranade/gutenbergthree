2b18decf0ee88b78e636d378408bc0a2df901fe9
Pranjali Ranade <ranadepranjali@gmail.com>
2023-06-08 17:05:43 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import queryString from "query-string";
+import {Route, withRouter} from "react-router-dom";
+import HomePage from "./components/handler/home-page";
+import BookList from "./components/handler/book-list";
+import { Genres, URL } from "./components/enums";
+import "typeface-roboto";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    genres: Genres,
+    booksLoadingStarted: false
+  }
+
+  setGenreHandler = (genre) => {
+    const {
+      history,
+      location: { search: searchString }
+    } = this.props;
+
+    const searchObj = {
+      ...queryString.parse(searchString),
+      topic: genre,
+      mime_type: "image/jpeg"
+    };
+
+    const query = queryString.stringify(searchObj);
+
+    history.push(`/books?${query}`);
+  };
+
+  render() {
+    const { genres } = this.state;
+
+    const bookList = () => <BookList url={URL} />;
+    const homePage = () => (
+      <HomePage genres={genres} setGenre={this.setGenreHandler} />
+    );
+
+    return (
+      <React.Fragment>
+        <Route exact path="/" component={homePage} />
+        <Route path="/books" component={bookList} />
+      </React.Fragment>
+    );
+  }
 }
 
-export default App;
+export default withRouter(App);
